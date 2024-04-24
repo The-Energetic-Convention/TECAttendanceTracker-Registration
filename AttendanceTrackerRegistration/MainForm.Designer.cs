f10b2ef6806419b9af76948f2ffdc68770895259
Unifox <awsomejojop@gmail.com>
2024-04-24 01:30:41 -0700

    fix

---
+++ AttendanceTrackerRegistration/MainForm.Designer.cs
@@ -0,0 +1,201 @@
+﻿using System.Drawing;
+
+namespace AttendanceTrackerRegistration
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.MainPanel = new System.Windows.Forms.Panel();
+            this.StatusText = new System.Windows.Forms.Label();
+            this.RegisterButton = new System.Windows.Forms.Button();
+            this.AtCon = new System.Windows.Forms.CheckBox();
+            this.NameLabel = new System.Windows.Forms.Label();
+            this.AttendeeName = new System.Windows.Forms.TextBox();
+            this.HeaderPanel = new System.Windows.Forms.Panel();
+            this.HeaderText = new System.Windows.Forms.Label();
+            this.MainPanel.SuspendLayout();
+            this.HeaderPanel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // MainPanel
+            // 
+            this.MainPanel.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.MainPanel.AutoSize = true;
+            this.MainPanel.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.MainPanel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(242)))), ((int)(((byte)(15)))), ((int)(((byte)(15)))), ((int)(((byte)(15)))));
+            this.MainPanel.Controls.Add(this.StatusText);
+            this.MainPanel.Controls.Add(this.RegisterButton);
+            this.MainPanel.Controls.Add(this.AtCon);
+            this.MainPanel.Controls.Add(this.NameLabel);
+            this.MainPanel.Controls.Add(this.AttendeeName);
+            this.MainPanel.Location = new System.Drawing.Point(12, 97);
+            this.MainPanel.Name = "MainPanel";
+            this.MainPanel.Size = new System.Drawing.Size(955, 525);
+            this.MainPanel.TabIndex = 0;
+            // 
+            // StatusText
+            // 
+            this.StatusText.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.StatusText.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(15)))), ((int)(((byte)(15)))), ((int)(((byte)(15)))));
+            this.StatusText.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.StatusText.Location = new System.Drawing.Point(47, 254);
+            this.StatusText.Name = "StatusText";
+            this.StatusText.Size = new System.Drawing.Size(855, 244);
+            this.StatusText.TabIndex = 4;
+            this.StatusText.Text = "Enter Info";
+            this.StatusText.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // RegisterButton
+            // 
+            this.RegisterButton.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.RegisterButton.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.RegisterButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(200)))), ((int)(((byte)(73)))), ((int)(((byte)(66)))), ((int)(((byte)(156)))));
+            this.RegisterButton.FlatAppearance.BorderSize = 0;
+            this.RegisterButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.RegisterButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.RegisterButton.ForeColor = System.Drawing.Color.White;
+            this.RegisterButton.Location = new System.Drawing.Point(342, 165);
+            this.RegisterButton.Name = "RegisterButton";
+            this.RegisterButton.Size = new System.Drawing.Size(260, 57);
+            this.RegisterButton.TabIndex = 3;
+            this.RegisterButton.Text = "Register";
+            this.RegisterButton.UseVisualStyleBackColor = false;
+            this.RegisterButton.Click += new System.EventHandler(this.RegisterButton_Click);
+            // 
+            // AtCon
+            // 
+            this.AtCon.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.AtCon.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(15)))), ((int)(((byte)(15)))), ((int)(((byte)(15)))));
+            this.AtCon.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.AtCon.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.AtCon.Location = new System.Drawing.Point(425, 110);
+            this.AtCon.Name = "AtCon";
+            this.AtCon.Size = new System.Drawing.Size(97, 29);
+            this.AtCon.TabIndex = 2;
+            this.AtCon.Text = "At Con";
+            this.AtCon.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.AtCon.UseVisualStyleBackColor = false;
+            // 
+            // NameLabel
+            // 
+            this.NameLabel.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.NameLabel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(15)))), ((int)(((byte)(15)))), ((int)(((byte)(15)))));
+            this.NameLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.NameLabel.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.NameLabel.Location = new System.Drawing.Point(47, 17);
+            this.NameLabel.Name = "NameLabel";
+            this.NameLabel.Size = new System.Drawing.Size(855, 25);
+            this.NameLabel.TabIndex = 1;
+            this.NameLabel.Text = "Attendee Name";
+            this.NameLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // AttendeeName
+            // 
+            this.AttendeeName.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.AttendeeName.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(73)))), ((int)(((byte)(66)))), ((int)(((byte)(156)))));
+            this.AttendeeName.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.AttendeeName.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.AttendeeName.ForeColor = System.Drawing.Color.White;
+            this.AttendeeName.Location = new System.Drawing.Point(47, 45);
+            this.AttendeeName.Name = "AttendeeName";
+            this.AttendeeName.Size = new System.Drawing.Size(855, 46);
+            this.AttendeeName.TabIndex = 0;
+            this.AttendeeName.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // HeaderPanel
+            // 
+            this.HeaderPanel.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.HeaderPanel.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.HeaderPanel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(242)))), ((int)(((byte)(15)))), ((int)(((byte)(15)))), ((int)(((byte)(15)))));
+            this.HeaderPanel.Controls.Add(this.HeaderText);
+            this.HeaderPanel.Location = new System.Drawing.Point(15, 12);
+            this.HeaderPanel.Name = "HeaderPanel";
+            this.HeaderPanel.Size = new System.Drawing.Size(949, 60);
+            this.HeaderPanel.TabIndex = 1;
+            // 
+            // HeaderText
+            // 
+            this.HeaderText.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.HeaderText.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(15)))), ((int)(((byte)(15)))), ((int)(((byte)(15)))));
+            this.HeaderText.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.HeaderText.Location = new System.Drawing.Point(3, 0);
+            this.HeaderText.Name = "HeaderText";
+            this.HeaderText.Size = new System.Drawing.Size(891, 60);
+            this.HeaderText.TabIndex = 0;
+            this.HeaderText.Text = "The Energetic Convention";
+            this.HeaderText.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(21)))), ((int)(((byte)(25)))), ((int)(((byte)(29)))));
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.ClientSize = new System.Drawing.Size(979, 634);
+            this.Controls.Add(this.HeaderPanel);
+            this.Controls.Add(this.MainPanel);
+            this.DoubleBuffered = true;
+            this.ForeColor = System.Drawing.Color.White;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Registration";
+            this.MainPanel.ResumeLayout(false);
+            this.MainPanel.PerformLayout();
+            this.HeaderPanel.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel MainPanel;
+        private System.Windows.Forms.Panel HeaderPanel;
+        private System.Windows.Forms.Label HeaderText;
+        private System.Windows.Forms.TextBox AttendeeName;
+        private System.Windows.Forms.Label NameLabel;
+        private System.Windows.Forms.Button RegisterButton;
+        private System.Windows.Forms.CheckBox AtCon;
+        private System.Windows.Forms.Label StatusText;
+    }
+}
+
---
+++ AttendanceTrackerRegistration/MainForm.cs
@@ -0,0 +1,150 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+using System.Diagnostics;
+using System.Threading;
+using Sydesoft.NfcDevice;
+
+namespace AttendanceTrackerRegistration
+{
+    public partial class MainForm : Form
+    {
+        HttpClient client = new HttpClient { BaseAddress = new Uri("http://10.0.0.216:6969") };
+        private string authKey = Environment.GetEnvironmentVariable("TECMasterAuth") ?? "NULL!";
+        ACR122U Reader = new ACR122U();
+
+        bool readyForCard = false;
+        bool readyToRegister = false;
+        bool writeDone = true;
+        string NameToWrite = "";
+        int IDToWrite = 0;
+
+        public MainForm()
+        {
+            InitializeComponent();
+            Reader.Init(false, 50, 4, 4, 200);
+            Reader.CardInserted += WriteBadge;
+            Reader.CardRemoved += BadgeRemoved;
+        }
+
+        private async void RegisterButton_Click(object sender, EventArgs e)
+        {
+            new Thread(Register).Start();
+        }
+
+        private async void Register()
+        {
+            //check to make sure a name is entered lol
+            if (AttendeeName.Text == "") { StatusText.Invoke((MethodInvoker)delegate { StatusText.Text = "No Name Entered!"; }); return; }
+
+            StatusText.Invoke((MethodInvoker)delegate { StatusText.Text = "Checking..."; });
+
+            // Make a request to the server to get the list of attendees
+            string attendeeListString = await client.GetStringAsync("/GetAttendee");
+            //StatusText.Invoke((MethodInvoker)delegate { StatusText.Text = attendeeListString; });
+            // Check to see if this one is already registered
+            Attendee[] attendeeList = JsonConvert.DeserializeObject<Attendee[]>(attendeeListString);
+            try
+            {
+                // if this gives an exception, the attendee name was not found
+                // if it doesnt, they were found, say so and return, no need to continue registration
+                Attendee found = attendeeList.Single(attendee => attendee.Name == AttendeeName.Text);
+                AttendeeName.Invoke((MethodInvoker)delegate { AttendeeName.Text = ""; });
+                StatusText.Invoke((MethodInvoker)delegate { StatusText.Text = "Attendee Name Already Registered!"; });
+                return;
+            }
+            catch { }
+            //StatusText.Invoke((MethodInvoker)delegate { StatusText.Text = "Not found"; });
+
+            //wait for badge writing, easier to rewrite the badge if the reqest fails, than delete the registration if a write fails
+            int newID = attendeeList[attendeeList.Length - 1].ID + 1;
+            NameToWrite = AttendeeName.Text;
+            IDToWrite = newID;
+            readyForCard = true;
+            StatusText.Invoke((MethodInvoker)delegate { StatusText.Text = "Scan Tag"; });
+
+            Console.WriteLine("spin spin spin spin");
+            while (!readyToRegister) { /* Camellia - SPIN ETERNALLY starts playing */ }
+
+            StatusText.Invoke((MethodInvoker)delegate { StatusText.Text = "Registering..."; });
+
+            // If not registered, make a request to register
+            string result = await client.PostAsync($"/RegisterAttendee?Name={AttendeeName.Text}&AtCon={AtCon.Checked}", new MultipartFormDataContent{
+                    new StringContent(authKey) { Headers = {
+                        ContentDisposition =
+                            new ContentDispositionHeaderValue("form-data") { Name = "Auth"} } } })
+                                .Result.Content.ReadAsStringAsync();
+            if (result != "SUCCESS") { StatusText.Invoke((MethodInvoker)delegate { StatusText.Text = $"Error: {result}"; }); return; }
+
+            // Make a request to get the Attendee, to check successful registration
+            string registeredAttendeeString = await client.GetStringAsync($"/GetAttendee?ID={attendeeList.Last().ID + 1}");
+            if (registeredAttendeeString == "[null]") { StatusText.Invoke((MethodInvoker)delegate { StatusText.Text = "Error Registering Attendee"; }); return; }
+            Attendee registeredAttendee = JsonConvert.DeserializeObject<Attendee[]>(registeredAttendeeString)[0];
+
+            StatusText.Invoke((MethodInvoker)delegate { StatusText.Text = "Done!"; });
+            readyToRegister = false;
+            Thread.Sleep(1000);
+
+            //reset ui
+            StatusText.Invoke((MethodInvoker)delegate { StatusText.Text = "Enter Info"; });
+            AttendeeName.Invoke((MethodInvoker)delegate { AttendeeName.Text = ""; });
+        }
+
+        void WriteBadge(PCSC.ICardReader reader)
+        {
+            if (readyForCard) {
+                writeDone = false;
+                readyForCard = false;
+                byte[] data = { 0x03, 0x25, 0xD1, 0x01, 0x21, 0x55, 0x00};
+                string toWrite = $"attendee://Name={NameToWrite}&ID={IDToWrite}";
+                bool result = Reader.WriteData(reader, data.Concat(Encoding.UTF8.GetBytes(toWrite)).ToArray());
+                if (!result) { StatusText.Invoke((MethodInvoker)delegate { StatusText.Text = "Error Writing badge"; }); }
+                string check = Encoding.UTF8.GetString(Reader.ReadData(reader));
+                if (check != toWrite) { StatusText.Invoke((MethodInvoker)delegate { StatusText.Text = "Error Writing Badge"; }); }
+                writeDone = true;
+                readyToRegister = true;
+                return;
+            }
+            StatusText.Invoke((MethodInvoker)delegate { StatusText.Text = "Not Ready For Badge"; });
+        }
+
+        void BadgeRemoved()
+        {
+            if (!writeDone)
+            {
+                StatusText.Invoke((MethodInvoker)delegate
+                {
+                    StatusText.Text = "Badge Removed Early!\nPlease wait for writing to be done!";
+                });
+            }
+        }
+    }
+
+    public class Attendee
+    {
+        public Attendee(string name, int iD, bool atCon, List<DateTime> joinDates, List<DateTime> leaveDates)
+        {
+            Name = name;
+            ID = iD;
+            AtCon = atCon;
+            JoinDates = joinDates;
+            LeaveDates = leaveDates;
+        }
+
+        public string Name { get; set; }
+        public int ID { get; set; }
+        public bool AtCon { get; set; }
+        public List<DateTime> JoinDates { get; set; }
+        public List<DateTime> LeaveDates { get; set; }
+    }
+}
